1ff539e7b7b3933f3ff4b71660cfc2953297d3ad
Nikita Beloglazov <nikelandjelo@gmail.com>
2013-10-14 20:33:10 +0100

    Add server logic for snake.

--- src/clj/multiplay/game/core.clj
+++ src/clj/multiplay/game/core.clj
@@ -1,30 +1,112 @@
 (ns multiplay.game.core
-  (:require [multiplay.game.params :as params]))
+  (:require [multiplay.game.params :as params]
+            [clojure.set :refer [difference]]))
 
-(defn- initial-player-state
-  [id name]
-  { :position [(+ 30 (rand-int params/game-width)) (+ 30 (rand-int params/game-height))]
-    :id id
-    :name name
-    :score 0})
+(def field-width 40)
+(def field-height 30)
+(def number-of-apples 5)
+
+
+(def initial-game-state
+   {:apples #{}
+    :snakes []
+    :walls #{[20 20] [30 30]}})
+
+(def dirs {:right [1 0]
+           :left [-1 0]
+           :up [0 -1]
+           :down [0 1]})
+
+(defn random-color
+  ([]
+     (->> #(+ 127 (rand-int 127))
+          (repeatedly 3)
+          vec))
+  ([forbidden]
+     (let [colors (repeatedly #(random-color))
+           forbidden (set forbidden)]
+       (->> colors distinct (remove forbidden) first))))
+
+(defn neib-cell [cell dir]
+  (let [[new-x new-y] (map + cell (dirs dir))]
+    [(mod (+ new-x field-width) field-width)
+     (mod (+ new-y field-height) field-height)]))
+
+(defn rand-cells [occupied]
+  (->> #(vector (rand-int field-width) (rand-int field-height))
+       repeatedly
+       (remove occupied)
+       distinct))
+
+(defn move-snake [{:keys [body dir] :as snake} apples]
+  (let [new-head (neib-cell (first body) dir)
+        new-body (if (apples new-head)
+                   (cons new-head body)
+                   (cons new-head (butlast body)))]
+    (assoc snake :body new-body)))
+
+(defn update-apples [snakes walls apples]
+  (let [heads (set (map #(first (:body %)) snakes))
+        apples (difference apples heads)
+        to-add (- number-of-apples (count apples))]
+    (if (pos? to-add)
+      (let [occupied (set (concat apples walls (mapcat :body snakes)))]
+        (->> (rand-cells occupied)
+         (take to-add)
+         (into apples)))
+      apples)))
+
+(defn update-world [world]
+  (let [{:keys [snakes apples walls]} world
+        new-snakes (map #(move-snake % apples) snakes)
+        occupied (frequencies (concat walls
+                                      (mapcat :body new-snakes)))
+        dead? (fn [snake] (> (occupied (first (:body snake))) 1))
+        alive (remove dead? new-snakes)]
+    {:walls walls
+     :snakes (vec alive)
+     :apples (update-apples alive walls apples)}))
+
+(defn add-snake [world id]
+  (let [{:keys [apples walls snakes]} world
+        occupied (set (concat apples walls (mapcat :body snakes)))
+        snake-head (first (rand-cells occupied))
+        colors (map :color snakes)
+        snake {:body [snake-head]
+               :dir :down
+               :id id
+               :color (random-color colors)}]
+    (update-in world [:snakes] conj snake)))
+
+(defn remove-snake [world id]
+  (letfn [(remove-by-id [snakes]
+            (remove #(= id (:id %)) snakes))]
+   (update-in world [:snakes] remove-by-id)))
+
+#_(defn find-snake-idx [id]
+  (let [snake-ids (vec (keep :id (:snakes @world)))
+        idx (.indexOf snake-ids id)]
+    (if (= idx -1) nil idx)))
+
+#_(defn change-snake-dir [world dir id]
+  (if-let [idx (find-snake-idx id)]
+    (assoc-in world [:snakes (find-snake-idx id) :dir] dir)))
 
 
 (defn move-up
-  [{[x y] :position :as player}]
-    (assoc player :position [x (dec y)]))
+  [snake]
+    (assoc snake :dir :up))
 
-(defn move-down 
-  [{[x y] :position :as player}]
-    (assoc player :position [x (inc y)]))
+(defn move-down
+  [snake]
+    (assoc snake :dir :down))
 
-(defn update-player [player-to-update action {:keys [players]}]
-  (map (fn [{:keys [id] :as player}]
+(defn update-player [player-to-update action {:keys [snakes]}]
+  (map (fn [{:keys [id] :as snake}]
     (if (= id player-to-update)
-      (action player)
-      player)) players))
+      (action snake)
+      snake)) snakes))
 
-(def initial-game-state
-  {:players []})
 
 (defmulti handle-command
   (fn [game-state command]
@@ -43,7 +125,7 @@
 
 (defmethod handle-command :player/join
   [game-state [command id name]]
-  (update-in game-state [:players] conj (initial-player-state id name)))
+  (add-snake game-state id))
 
 (defmethod handle-command :player/up
   [game-state [command id]]
@@ -67,4 +149,4 @@
   [game-state commands]
   (let [new-game-state (-> game-state
                            (handle-commands commands))]
-    new-game-state))
+    (update-world new-game-state)))
